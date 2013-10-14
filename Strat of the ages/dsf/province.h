9e82ab1f708577be9d96f56ec66571666cabf1e0
bandw2 <bandw2gs@hotmail.com>
2013-10-14 14:28:48 -0700

    fixed province graph

--- Strat of the ages/dsf/province.h
+++ Strat of the ages/dsf/province.h
@@ -10,10 +10,10 @@ struct Province{
 	float mManpower;
 	Nation* m_Nation;
 	Province(int atype,long long aID,float aTax, float aManpower):mtype(atype),mID(aID),mTax(aTax),mManpower(aManpower),m_Nation(NULL){//don't try to understand this if you don't want to.
-		aID%500 == 0 ? connections[0]=NULL : connections[0] = aID-1;
-		aID/500 == 0 ? connections[1]=NULL, connections[2]=NULL : (aID/500)%2 == 0 ? connections[1]=aID-501, connections[2]=aID-500 : connections[1]=aID-500, connections[2]=aID-499 ;
-		aID%500 == 499 ? connections[3]=NULL : connections[3]=aID+1;
-		aID/500 == 499 ? connections[4]=NULL, connections[5]=NULL : (aID/500)%2 == 0 ? connections[5]=aID+499, connections[4]=aID+500 : connections[5]=aID+500, connections[4]=aID+501 ;
+		aID%100 == 0 ? connections[0]=NULL : connections[0] = aID-1;
+		aID/100 == 0 ? connections[1]=NULL, connections[2]=NULL : (aID/100)%2 == 0 ? connections[1]=aID-101, connections[2]=aID-100 : connections[1]=aID-100, connections[2]=aID-99 ;
+		aID%100 == 99 ? connections[3]=NULL : connections[3]=aID+1;
+		aID/100 == 99 ? connections[4]=NULL, connections[5]=NULL : (aID/100)%2 == 0 ? connections[5]=aID+99, connections[4]=aID+100 : connections[5]=aID+100, connections[4]=aID+101 ;
 			
 	}
 };
